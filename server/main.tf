d4198e2b7cd34e1a96185bd7ac9deca8f45616d6
murugesh <murugesh3001@gmail.com>
2022-11-02 17:21:36 +0530

    minor

--- server/main.tf
+++ server/main.tf
@@ -56,3 +56,11 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my_first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.first_service.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+}
