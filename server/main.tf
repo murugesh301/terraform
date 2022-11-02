e88a7ee1635990640bb4837950295d77c3f10dd9
murugesh <murugesh3001@gmail.com>
2022-11-02 15:20:05 +0530

    changes

--- server/main.tf
+++ server/main.tf
@@ -10,37 +10,33 @@ resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster" 
 }
 
-resource "aws_ecs_task_definition" "first_task" {
-  family                   = "first-task" # Naming our first task
-  container_definitions    = <<DEFINITION
-  [
+resource "aws_ecs_task_definition" "first_service" {
+  family = "first_service"
+  container_definitions = definition([
     {
-      "name": "first-task",
-      "image": "${434828451347.dkr.ecr.ap-south-1.amazonaws.com/my-ecr-repo}"
-      "essential": true,
-      "portMappings": [
+      name      = "first"
+      image     = "${434828451347.dkr.ecr.ap-south-1.amazonaws.com/my-ecr-repo}"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
         {
-          "containerPort": 3000,
-          "hostPort": 3000
+          containerPort = 80
+          hostPort      = 80
         }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+      ]
+    },
+DEFINITION
+requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
-
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
-
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -51,7 +47,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
     }
   }
 }
-
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
