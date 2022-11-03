5235390a46dbbe81fb2ed162b7ef2f3491de181b
murugesh <murugesh3001@gmail.com>
2022-11-03 10:27:06 +0530

    minor

--- server/main.tf
+++ server/main.tf
@@ -64,3 +64,24 @@ resource "aws_ecs_service" "my_first_service" {
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
 }
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "ap-south-1c"
+}
+
+
+
+
